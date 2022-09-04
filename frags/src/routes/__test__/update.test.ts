ee5907beb4dc96832a52de2529b6e8ac5c481e85
Bricz <bricz.cruz@gmail.com>
2022-09-03 20:58:40 -0500

    finished frag update routes with tests

---
+++ frags/src/routes/__test__/update.test.ts
@@ -0,0 +1,95 @@
+import request from 'supertest';
+import { app } from '../../app';
+import mongoose from 'mongoose';
+
+it('returns a 404 if the provided id does not exist', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString(); //some random valid id instead random gibberish like "sdjfsadjsajdksla"
+  await request(app)
+    .put(`/api/frags/${id}`)
+    .set('Cookie', global.getSignInCookie())
+    .send({
+      title: 'aslkdfj',
+      price: 20,
+    })
+    .expect(404);
+});
+
+it('returns a 401 if the user is not authenticated', async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+  await request(app)
+    .put(`/api/frags/${id}`)
+    .send({
+      title: 'aslkdfj',
+      price: 20,
+    })
+    .expect(401);
+});
+
+it('returns a 401 if the user does not own the frags', async () => {
+  const res = await request(app)
+    .post('/api/frags')
+    .set('Cookie', global.getSignInCookie())
+    .send({
+      title: 'asldkfj',
+      price: 20,
+    });
+
+  await request(app)
+    .put(`/api/frags/${res.body.id}`)
+    .set('Cookie', global.getSignInCookie()) //regenerate a new random id to mismatch
+    .send({
+      title: 'alskdjflskjdf',
+      price: 1000,
+    })
+    .expect(401);
+});
+
+it('returns a 400 if the user provides an invalid title or price', async () => {
+  const cookie = global.getSignInCookie();
+
+  const res = await request(app).post('/api/frags').set('Cookie', cookie).send({
+    title: 'asldkfj',
+    price: 20,
+  });
+
+  await request(app)
+    .put(`/api/frags/${res.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: '',
+      price: 20,
+    })
+    .expect(400);
+
+  await request(app)
+    .put(`/api/frags/${res.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'alskdfjj',
+      price: -10,
+    })
+    .expect(400);
+});
+
+it('updates the frags provided valid inputs', async () => {
+  const cookie = global.getSignInCookie();
+
+  const res = await request(app).post('/api/frags').set('Cookie', cookie).send({
+    title: 'asldkfj',
+    price: 20,
+  });
+
+  await request(app)
+    .put(`/api/frags/${res.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'new title',
+      price: 100,
+    })
+    .expect(200);
+
+  const fragRes = await request(app).get(`/api/frags/${res.body.id}`).send();
+
+  expect(fragRes.body.title).toEqual('new title');
+  expect(fragRes.body.price).toEqual(100);
+});
---
+++ frags/src/routes/update.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  UnauthorizedError,
+} from '@abodeorg/common';
+import { Fragrance } from '../models/frag';
+
+const router = express.Router();
+
+router.put(
+  '/api/frags/:id',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be greater than 0!'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const frag = await Fragrance.findById(req.params.id);
+
+    if (!frag) {
+      throw new NotFoundError();
+    }
+
+    console.log(frag.userId);
+    console.log(req.currentUser!.id);
+
+    if (frag.userId !== req.currentUser!.id) {
+      throw new UnauthorizedError();
+    }
+
+    frag.set({
+      title: req.body.title,
+      price: req.body.price,
+    });
+    await frag.save();
+    res.send(frag);
+  }
+);
+
+export { router as updateFragRouter };
