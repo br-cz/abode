569265993138ab9be9fa5f3769edadd12348536d
Bricz <bricz.cruz@gmail.com>
2022-09-03 20:59:15 -0500

    finished frags service, tested with postman along with common and skaffold files

---
+++ frags/src/app.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import { json } from 'body-parser';
+
+//import before routes
+import 'express-async-errors';
+
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@abodeorg/common';
+import { createFragRouter } from './routes/new';
+import { showFragRouter } from './routes/show';
+import { retrieveFragsRouter } from './routes/retrieve';
+import { updateFragRouter } from './routes/update';
+
+const app = express();
+//traffic to our app is being proxy'd through ingress and
+//since we're only sending cookies when users are connected through https
+//we let express know it can trust that proxy
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    //don't need to encrypt cookies because JWTs are tamper resistant
+    //and this allows cookies to be easily read across multiple langs
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test', //allows unsecure requests during testing
+  })
+);
+
+//should be ran after cookieSession so it can set the req.session property properly
+app.use(currentUser);
+app.use(createFragRouter);
+app.use(showFragRouter);
+app.use(retrieveFragsRouter);
+app.use(updateFragRouter);
+
+//send not found error for all undefined routes
+app.all('*', async (req, res) => {
+  // console.log('added express async errors');
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ frags/src/index.ts
@@ -0,0 +1,27 @@
+import { app } from './app';
+import mongoose from 'mongoose';
+
+const db = async () => {
+  //so TS doesn't throw an error about a possibly undefined env variable
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY not found');
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI not found');
+  }
+
+  try {
+    //auth-mongo-serv:27107 is the domain mongoose will connect to
+    // /auth is the name of the db mongoose will create for us
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log('Connected to mongo');
+  } catch (e) {
+    console.log(e);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!');
+  });
+};
+
+db();
