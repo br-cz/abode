83180fbe2b603206d722f2ade6fc03fcb57192f8
Bricz <bricz.cruz@gmail.com>
2022-10-18 18:06:01 -0500

    testing specified tests

--- frags/src/index.ts
+++ frags/src/index.ts
@@ -5,8 +5,6 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 import { OrderCancelledListener } from './events/listeners/order-cancelled-listener';
 
 const db = async () => {
-  console.log('Testing workflow');
-
   //so TS doesn't throw an error about a possibly undefined env variable
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY not found');
