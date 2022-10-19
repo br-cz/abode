92bb9a7b1c5b4deeb0b79c287a55a4b2eef1f3d5
Bricz <bricz.cruz@gmail.com>
2022-10-18 21:26:32 -0500

    lets test the deployment lets goooo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import { app } from './app';
 import mongoose from 'mongoose';
 
 const db = async () => {
-  console.log('testing ci/cd workflow');
+  console.log('Deployed on Digital Ocean!');
 
   //so TS doesn't throw an error about a possibly undefined env variable
   if (!process.env.JWT_KEY) {
